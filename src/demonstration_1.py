a2db4b459ff30d4ff81985bcde881acae029dd1d
Vish Singh <vishalicious213@gmail.com>
2020-11-19 20:38:24 -0800

    added solutions from lecture

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,3 +1,5 @@
+# recording from lecture: https://youtu.be/51xj4tETg6E
+
 """
 Your task is create your own HashTable without using a built-in library.
 
@@ -29,20 +31,55 @@ class ListNode:
         self.key = key
         self.value = value
         self.next = None
-
+​
+​
 class MyHashTable:
     def __init__(self):
-        # Your code here
-
-
+        self.array = [None] * 20
+​
     def put(self, key, value):
-        # Your code here
-
-
+        # We need an array to store the values in
+        # Need to turn the key to index
+        # Hash the key (map it to an integer in a finite space)
+        hashed_key = hash(key)
+        # Map the hashed key to an index in the array by % len(array)
+        index = hashed_key % len(self.array)
+        # Put the value in the array at that index
+        self.array[index] = value
+​
     def get(self, key):
-        # Your code here
-
-
-    def remove(self, key: int) -> None:
-        # Your code here
-
+        # PLAN: return the value at the index of the key
+        # hash the key, map it to the index, and then return the value at that index in the array
+        hashed_key = hash(key)
+        index = hashed_key % len(self.array)
+        if self.array[index] is None:
+            return -1
+        return self.array[index]
+​
+    def remove(self, key):
+        hashed_key = hash(key)
+        index = hashed_key % len(self.array)
+​
+        self.array[index] = None
+​
+​
+hash_table = MyHashTable()
+hash_table.put("a", 1)
+hash_table.put("b", 2)
+print(hash_table.get("a"))  # returns 1
+print(hash_table.get("b"))  # returns 2
+print(hash_table.get("c"))  # returns - 1(not found)
+​
+hash_table.put("b", 1)  # update the existing value
+print(hash_table.get("b"))  # returns 1
+hash_table.remove("b")  # remove the mapping for 2
+print(hash_table.get("b"))  # returns - 1(not found)
+​
+​
+hash_table_2 = MyHashTable()
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for char in alphabet:
+    hash_table_2.put(char, char)
+​
+for char in alphabet:
+    print(char, hash_table_2.get(char))
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -56,3 +56,59 @@ def are_words_sorted(words, alpha_order):
     """
     # Your code here
 
+    # UNDERSTAND
+​
+    # Plan
+    # compare pairs of words to make sure word 1 < word 2
+        # compare every letter in each word to the alphabet and then compare them to each other to determine if the order is correct
+    # each letter in the new alphabet needs a numeric value so we know what comes first, second, etc. --> dict (hash table)
+​
+    # Step 1: map each letter to numeric value in store in dict
+    # Runtime: O(len(alpha_order)) = O(~26) = O(1)
+    alphabet_dict = {}
+    for i in range(len(alpha_order)):
+        character = alpha_order[i]
+        alphabet_dict[character] = i
+    print(alphabet_dict)
+    # Step 2:
+        # iterate through words and compare pairs of words to see if they're in sorted order
+    for word_index in range(len(words) - 1):  # O(len(words))
+        # compare the words
+        word_a = words[word_index]
+        word_b = words[word_index + 1]
+​
+        for letter_index in range(len(word_a)):  # O(len(word_a))
+            char_a = word_a[letter_index]
+            if letter_index >= len(word_b):
+                # word b is a substring of word a, e.g., "lambda" / "lam"
+                return False
+            char_b = word_b[letter_index]
+            # compare the first letters
+            index_a = alphabet_dict[char_a]     # O(1) because of hashtables
+            index_b = alphabet_dict[char_b]
+            #  if they're the same, compare the next two letters,
+            if index_a == index_b:
+                continue
+            # if they're different, we know which one comes first / second.
+            if index_a < index_b:
+                # word_a < word_b, break out of the loop comparing letters and compare the next pair of words
+                break
+            if index_a > index_b:
+                # if not, return false
+                # word_a > word_b, so the words are out of order. return False
+                print(letter_index, char_a, char_b)
+                return False
+            #  repeat.
+    # Step 3: return true
+    return True
+​
+​
+print(are_words_sorted(["apple", "app"], "hlabcdefgijkmnopqrstuvwxyz"))
+​
+# # Dictionary comprehension:
+# alpha = "abcdefg"
+# dictionary = {character: alpha.index(character) for character in alpha}
+#
+# d2 = {}
+# for character in alpha:
+#     d2[character] = alpha.index(character)
